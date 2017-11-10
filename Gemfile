16f71bd9b4c7625d69025744b4018aef3efbe598
Diego Bonfim <fbm.diego@gmail.com>
2017-11-10 17:45:00 -0200

    Add Rspec config

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ gem 'mongoid'
 gem 'sinatra-contrib'
 gem 'unicorn'
 gem 'require_all'
+
+group :test do
+  gem 'rspec'
+end
---
+++ spec/lib/helpers/book_serializer_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+=begin
+RSpec.describe BookList::Helpers::BookSerializer do
+  describe "#as_json" do
+    it "generate json to book" do
+      book = Book.new
+      book.id = 123
+      expect(book.as_json).to eq("")
+    end
+  end
+end
+=end
